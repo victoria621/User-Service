75cb45df751fc0b679967bea4a0942ea245edb11
victoria <vasharchuk07@gmail.com>
2026-05-17 17:18:06 +0300

    add DTO

---
+++ userService/src/main/java/com/innowise/userService/dto/UserRequestDTO.java
@@ -0,0 +1,22 @@
+package com.innowise.userService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public record UserRequestDTO(
+        @NotBlank(message = "Name is required")
+        String name,
+        @NotBlank(message = "Surname is required")
+        String surname,
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        @Past(message = "Birth date must be in the past")
+        LocalDate birthDate,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email
+) {
+}
