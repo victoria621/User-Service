f7f85a5aa84aa848a2574a64560edbedf0abeff7
victoria <vasharchuk07@gmail.com>
2026-05-20 12:25:15 +0300

    Remove unused import

--- userService/src/main/java/com/innowise/user_service/exception/ResourceNotFoundException.java
+++ userService/src/main/java/com/innowise/user_service/exception/ResourceNotFoundException.java
@@ -1,9 +1,5 @@
 package com.innowise.user_service.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 public class ResourceNotFoundException extends RuntimeException  {
     public ResourceNotFoundException(String message) {
         super(message);
